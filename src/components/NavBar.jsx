0cb02e4d6c18afc7a3df99c22e92a613b2a1f9cc
youngnamh <youngnamhlady@gmail.com>
2023-07-26 21:22:12 -0400

    cart icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import cart from "../assets/cart-shopping-solid.svg";
 
 const NavBar = () => {
   return (
-    <div className="w-full flex justify-center bg-green-500 p-2">
-      <div className="flex justify-between w-1/2 text-white">
+    <div className="w-full flex justify-between bg-black opacity-80 p-2  text-white">
+      <div className="p-2 text-2xl font-bold">Kurita Co.</div>
+      <div className="flex justify-between w-1/2">
         <div className="border-l-2 border-white h-full"></div>
         <Link to="/" className="navIcon">
           Home
@@ -14,7 +16,7 @@ const NavBar = () => {
         </Link>
         <div className="border-l-2 border-white h-full"></div>
         <Link to="/cart" className="navIcon">
-          Cart
+          <img src={cart} className=" h-5 w-5 fill-white" alt="cart logo" />
         </Link>
         <div className="border-l-2 border-white h-full"></div>
       </div>
