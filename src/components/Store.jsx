42ed01f1f467922175e45c4bc9bb9bd4136a95e6
youngnamh <youngnamhlady@gmail.com>
2023-08-02 11:20:17 -0400

    fetch

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,4 +1,34 @@
+import { useEffect, useState } from "react";
+
 const Store = () => {
+  const [products, setProducts] = useState(null);
+  const [toggle, setToggle] = useState(true);
+
+  const createListings = () => {
+    if (toggle) {
+      setToggle(false);
+      for (let i = 0; i < products.length; i++) {
+        console.log(products[i]);
+        console.log(i);
+      }
+    } else {
+      console.log("already done");
+    }
+  };
+
+  //initial fetch
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/category/electronics")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  useEffect(() => {
+    if (products) {
+      createListings();
+    }
+  }, [products]);
+
   return <div>Store</div>;
 };
 
