1dcf984e7ef5544e8ebe470fbee972accb9eb703
youngnamh <youngnamhlady@gmail.com>
2023-08-06 21:28:26 -0400

    adding more to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,12 @@
 export default function Footer() {
-  return <div className="bg-black opacity-80 text-white">Footer</div>;
+  return (
+    <div className="bg-black opacity-80 text-white">
+      <div>
+        <div>faq</div>
+        <div>contact us</div>
+        <div>careers</div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
